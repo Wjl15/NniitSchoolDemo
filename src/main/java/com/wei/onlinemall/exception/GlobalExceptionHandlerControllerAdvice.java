cf041ee476e91001d6c2fc3ed207ca33c87c95f6
Wjl15 <1541558237@qq.com>
2022-09-30 16:15:25 +0800

    多表联动

--- src/main/java/com/wei/onlinemall/exception/GlobalExceptionHandlerControllerAdvice.java
+++ src/main/java/com/wei/onlinemall/exception/GlobalExceptionHandlerControllerAdvice.java
@@ -1,31 +1,31 @@
-//package com.wei.onlinemall.exception;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//import java.sql.SQLException;
-//
-///**
-// * @Author WeiJinLong
-// * @Date 2022-09-22 14:11
-// */
-////@ControllerAdvice
-////@ResponseBody
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandlerControllerAdvice {
-//    @ExceptionHandler(value = Exception.class)
-//    public String handlerException(@Autowired Exception ex){
-//        if (ex instanceof SQLException){
-//            return "发生了sql异常";
-//        }else if (ex instanceof BusinessException){
-//            return "发生了业务异常";
-//        }else {
-//            return "发生了未知异常";
-//        }
-//    }
-//
-//}
+package com.wei.onlinemall.exception;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLException;
+
+/**
+ * @Author WeiJinLong
+ * @Date 2022-09-22 14:11
+ */
+//@ControllerAdvice
+//@ResponseBody
+
+@RestControllerAdvice
+public class GlobalExceptionHandlerControllerAdvice {
+    @ExceptionHandler(value = Exception.class)
+    public String handlerException(@Autowired Exception ex){
+        if (ex instanceof SQLException){
+            return "发生了sql异常";
+        }else if (ex instanceof BusinessException){
+            return "发生了业务异常";
+        }else {
+            return "发生了未知异常";
+        }
+    }
+
+}
